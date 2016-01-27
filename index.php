4e0619b13e53c596be2780c3d5ca07207c96140b
Steven Jay <stevenjasionowicz@gmail.com>
2016-01-27 14:29:28 +1300

    Created templates

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Shopping Cart</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-<body>
-	<div id="body">
+<?php include 'templates/header.template.php'; ?>
+
+
 		<h1>Shopping Cart</h1>
 
 		<?php 
@@ -22,20 +17,10 @@
 			while( $row = $result->fetch_assoc() ) {
 
 				//Present the data 
-				echo '<ul>';
-				echo '<li>ID: '.$row['id'].' </li>';
-				echo '<li>Name: '.$row['name'].' </li>';
-				echo '<li>Description: '.$row['description'].' </li>';
-				echo '<li>Price: '.$row['price'].' </li>';
-				echo '<li>Stock: '.$row['stock'].' </li>';
-				echo '</ul>';
-				echo '<br>';
+				include 'templates/product.template.php';
 
 			}
 
 		?>
 
-
-	</div>
-</body>
-</html>
+<?php include 'templates/footer.template.php'; ?>
---
+++ templates/header.template.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shopping Cart</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div id="body">
